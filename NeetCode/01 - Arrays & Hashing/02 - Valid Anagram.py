43bf6a73e15eec72538e82055f67778566538cd9
george-ezat <e.georgeezat@gmail.com>
2025-08-03 08:16:54 +0300

    NeetCode-75 Daily Challenge - Day 2

---
+++ NeetCode/01 - Arrays & Hashing/02 - Valid Anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_cnt = {x: 0 for x in s}
+        t_cnt = {x: 0 for x in t}
+
+        for i in range(len(s)):
+            s_cnt[s[i]] += 1
+            t_cnt[t[i]] += 1
+
+        return s_cnt == t_cnt
+
+
+# Another Solution
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        return sorted(s) == sorted(t)
