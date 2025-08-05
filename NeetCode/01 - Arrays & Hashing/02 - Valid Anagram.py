32979d1a2086fe2e7de065ae9d981a2857f3594c
george-ezat <e.georgeezat@gmail.com>
2025-08-05 11:46:03 +0300

    Add another solution for problem 2: Using Counter data structure from collections Module

--- NeetCode/01 - Arrays & Hashing/02 - Valid Anagram.py
+++ NeetCode/01 - Arrays & Hashing/02 - Valid Anagram.py
@@ -21,3 +21,13 @@ class Solution:
             return False
 
         return sorted(s) == sorted(t)
+
+
+# Another Solution: (Using 'Counter' from collections Module)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_cnt = Counter(s)
+        t_cnt = Counter(t)
+
+        return s_cnt == t_cnt
